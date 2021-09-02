4c79ffaf0550cbf08fc181edbeaea39a024a1ac6
chenkang <chenkang@yalalat.com>
2021-09-02 10:24:19 +0800

    新增 add 命令

--- cmd/add.go
+++ cmd/add.go
@@ -21,23 +21,31 @@ import (
 )
 
 // addCmd represents the add command
+var Name string
 var spider = controller.Spider{}
 var addCmd = &cobra.Command{
-	Use:   "add",
-	Short: "添加一个目标url地址",
-	Long:  `添加一个url地址，将此地址加入到工作队列中，并同时生成这个地址爬虫规则文件。`,
-	Run:   spider.Add,
+	Use: "add",
+	// 定义arguments数量最少为1个
+	Args:  cobra.MinimumNArgs(1),
+	Short: "add ...urls",
+	Long:  `添加一个或多个url地址，多个用空格分割，将地址加入到工作队列中，并同时生成这个地址爬虫规则文件。`,
+	Run: func(cmd *cobra.Command, args []string) {
+		spider.Add(cmd, args, Name)
+	},
 }
 
 func init() {
 	rootCmd.AddCommand(addCmd)
 
+	//永久选项
 	// 下面定义了一个Flag foo, foo后面接的值会被赋值给Foo
 	//Foo = addCmd.PersistentFlags().String("foo", "", "A help for foo")
 	// 下面定义了一个Flag print ,print后面的值会被赋值给Print变量
 	//addCmd.PersistentFlags().StringVar(&Print, "print", "", "print")
 	// 下面定义了一个Flag show,show默认为false, 有两种调用方式--show\-s，命令后面接了show则上面定义的show变量就会变成true
 	//addCmd.PersistentFlags().BoolVarP(&show, "show", "s", false, "show")
+
+	//本地选项
 	// 下面定义了一个Flag show,show默认为false, 有两种调用方式--show\-s，命令后面接了show则上面定义的show变量就会变成true
 	//showL = *addCmd.Flags().BoolP("showL", "S", false, "show")
 	// 下面定义了一个Flag print ,print后面的值会被赋值给Print变量
@@ -47,5 +55,6 @@ func init() {
 	//show = *testCmd.Flags().BoolP("show", "s", false, "show")
 	// 设置使用test的时候后面必须接show
 	//_ = testCmd.MarkFlagRequired("show")
-
+	addCmd.Flags().StringVarP(&Name, "name", "n", "", "必须为url标记个名称，多个需要一一对应用逗号隔开")
+	_ = addCmd.MarkFlagRequired("name")
 }
--- controller/spider.go
+++ controller/spider.go
@@ -1,12 +1,21 @@
 package controller
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
 
 type Spider struct {
 	UrlList map[string]string
 }
 
 // 添加一个目标url地址
-func (s *Spider) Add(cmd *cobra.Command, args []string) {
-	println("被调用")
+func (s *Spider) Add(cmd *cobra.Command, args []string, globals ...string) {
+	for _, arg := range args {
+		fmt.Println("Arg:", arg)
+	}
+	for _, glo := range globals {
+		fmt.Println("glo:", glo)
+	}
 }
